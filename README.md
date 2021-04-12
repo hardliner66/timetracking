7dba8fd9a4b0016cf100d6704e53f72193f088be
hardliner66 <hardliner66@gmail.com>
2021-04-12 11:05:43 +0200

    fix clippy errors

---
+++ README.md
@@ -0,0 +1,25 @@
+# Timetracking
+
+Simple command line time tracking application.
+
+## Commandline
+```sh
+timetracking 1.0.0
+
+USAGE:
+    tt <SUBCOMMAND>
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+SUBCOMMANDS:
+    continue    start time tracking with last description
+    export      export the file as json
+    help        Prints this message or the help of the given subcommand(s)
+    list        list all entries
+    path        show path to data file
+    show        show work time for given timespan
+    start       start time tracking
+    stop        stop time tracking
+```
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::path::Path;
 use chrono::{prelude::*, serde::ts_seconds, Duration, NaiveDate, NaiveDateTime, NaiveTime};
 use serde::{Deserialize, Serialize};
+use std::path::Path;
+#[cfg(feature = "binary")]
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -42,6 +44,12 @@ enum Command {
         /// the stop time [defaults to start day 23:59:59]
         stop: Option<String>,
     },
+    #[cfg(feature = "binary")]
+    /// export the file as json
+    Export {
+        /// where to write the json file
+        path: PathBuf,
+    },
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -61,8 +69,7 @@ enum TrackingEvent {
 impl TrackingEvent {
     fn time(&self) -> DateTime<Utc> {
         match self {
-            Self::Start(TrackingData { time, .. }) => *time,
-            Self::Stop(TrackingData { time, .. }) => *time,
+            Self::Start(TrackingData { time, .. }) | Self::Stop(TrackingData { time, .. }) => *time,
         }
     }
 
@@ -87,36 +94,148 @@ enum DateOrDateTime {
     DateTime(NaiveDateTime),
 }
 
-#[cfg(feature="binary")]
+#[cfg(feature = "binary")]
 fn read_data<P: AsRef<Path>>(path: P) -> Vec<TrackingEvent> {
     let data = std::fs::read(&path).unwrap_or_default();
     bincode::deserialize(&data).unwrap_or_default()
 }
 
-#[cfg(not(feature="binary"))]
+#[cfg(not(feature = "binary"))]
 fn read_data<P: AsRef<Path>>(path: P) -> Vec<TrackingEvent> {
     let data = std::fs::read_to_string(&path).unwrap_or_default();
     serde_json::from_str(&data).unwrap_or_default()
 }
 
-#[cfg(feature="binary")]
-fn write_data<P: AsRef<Path>>(path: P, data: Vec<TrackingEvent>) {
-    let data = bincode::serialize(&data).expect("could not serialize data");
+#[cfg(feature = "binary")]
+fn write_data<P: AsRef<Path>>(path: P, data: &[TrackingEvent]) {
+    let data = bincode::serialize(data).expect("could not serialize data");
     std::fs::write(path, data).expect("could not write data file");
 }
 
-#[cfg(not(feature="binary"))]
-fn write_data<P: AsRef<Path>>(path: P, data: Vec<TrackingEvent>) {
-    let data = serde_json::to_string(&data).expect("could not serialize data");
+fn write_data_json<P: AsRef<Path>>(path: P, data: &[TrackingEvent]) {
+    let data = serde_json::to_string(data).expect("could not serialize data");
     std::fs::write(path, data).expect("could not write data file");
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[cfg(not(feature = "binary"))]
+fn write_data<P: AsRef<Path>>(path: P, data: &[TrackingEvent]) {
+    write_data_json(path, data);
+}
+
+fn start_tracking(data: &mut Vec<TrackingEvent>, description: Option<String>, at: Option<String>) {
+    let should_add = match data.last() {
+        None => true,
+        Some(event) => event.is_stop(),
+    };
+    if should_add {
+        data.push(TrackingEvent::Start(TrackingData {
+            description,
+            time: at.map_or_else(|| Local::now().into(), |at| parse_date_time(&at)),
+        }));
+    }
+}
+
+fn stop_tracking(data: &mut Vec<TrackingEvent>, description: Option<String>, at: Option<String>) {
+    let should_add = match data.last() {
+        None => true,
+        Some(event) => event.is_start(),
+    };
+    if should_add {
+        data.push(TrackingEvent::Stop(TrackingData {
+            description,
+            time: at.map_or_else(|| Local::now().into(), |at| parse_date_time(&at)),
+        }))
+    }
+}
+
+fn continue_tracking(data: &mut Vec<TrackingEvent>) {
+    if let Some(TrackingEvent::Stop { .. }) = data.last() {
+        if let Some(TrackingEvent::Start(TrackingData { description, .. })) =
+            data.iter().rev().find(|t| t.is_start()).cloned()
+        {
+            data.push(TrackingEvent::Start(TrackingData {
+                description,
+                time: Local::now().into(),
+            }))
+        }
+    } else {
+        eprintln!("Time tracking couldn't be continued, because there are no entries. Use the start command instead!");
+    }
+}
+
+fn show(data: &mut Vec<TrackingEvent>, start: Option<String>, stop: Option<String>) {
+    let start = start.map(|start| parse_date_or_date_time(&start));
+    let stop = match stop {
+        Some(s) if s == "all" => None,
+        Some(s) => Some(parse_date_or_date_time(&s)),
+        None => match start {
+            Some(DateOrDateTime::DateTime(start)) => Some(DateOrDateTime::Date(start.date())),
+            start => start,
+        },
+    };
+    let mut data_iterator = data
+        .iter()
+        .filter(|entry| match start {
+            None => true,
+            Some(DateOrDateTime::Date(start)) => {
+                (*entry).time().timestamp_millis()
+                    >= start
+                        .and_time(NaiveTime::from_hms(0, 0, 0))
+                        .timestamp_millis()
+            }
+            Some(DateOrDateTime::DateTime(start)) => {
+                entry.time().timestamp_millis() >= start.timestamp_millis()
+            }
+        })
+        .filter(|entry| match stop {
+            None => true,
+            Some(DateOrDateTime::Date(stop)) => {
+                entry.time().timestamp_millis()
+                    <= stop
+                        .and_time(NaiveTime::from_hms(23, 59, 59))
+                        .timestamp_millis()
+            }
+            Some(DateOrDateTime::DateTime(stop)) => {
+                entry.time().timestamp_millis() <= stop.timestamp_millis()
+            }
+        })
+        .skip_while(|entry| TrackingEvent::is_stop(entry));
+    let mut work_day = Duration::zero();
+    loop {
+        let start = data_iterator.next();
+        let stop = data_iterator.next();
+        match (start, stop) {
+            (Some(start), Some(stop)) => {
+                let duration = stop.time() - start.time();
+                work_day = work_day
+                    .checked_add(&duration)
+                    .expect("couldn't add up durations");
+            }
+            (Some(start), None) => {
+                let duration = Utc::now() - start.time();
+                work_day = work_day
+                    .checked_add(&duration)
+                    .expect("couldn't add up durations");
+                break;
+            }
+            (_, _) => break,
+        }
+    }
+    let hours = work_day.num_hours();
+    let hours_in_minutes = hours * 60;
+    let hours_in_seconds = hours_in_minutes * 60;
+    let minutes = work_day.num_minutes() - hours_in_minutes;
+    let minutes_in_seconds = minutes * 60;
+    let seconds = work_day.num_seconds() - hours_in_seconds - minutes_in_seconds;
+    println!("Work Time: {:02}:{:02}:{:02}", hours, minutes, seconds);
+}
+
+fn main() {
     let Options { command } = Options::from_args();
 
-    let mut path = dirs::home_dir().unwrap_or(".".into());
+    let mut path = dirs::home_dir().unwrap_or_else(|| ".".into());
 
-    if cfg!(feature="binary") {
+    if cfg!(feature = "binary") {
         path.push("timetracking.bin");
     } else {
         path.push("timetracking.json");
@@ -124,130 +243,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut data = read_data(&path);
 
-    use Command::*;
     match command {
-        Start { description, at } => {
-            let should_add = match data.last() {
-                None => true,
-                Some(event) => event.is_stop(),
-            };
-            if should_add {
-                data.push(TrackingEvent::Start(TrackingData {
-                    description,
-                    time: at.map(parse_date_time).unwrap_or(Local::now().into()),
-                }));
-            }
-        }
-        Stop { description, at } => {
-            let should_add = match data.last() {
-                None => true,
-                Some(event) => event.is_start(),
-            };
-            if should_add {
-                data.push(TrackingEvent::Stop(TrackingData {
-                    description,
-                    time: at.map(parse_date_time).unwrap_or(Local::now().into()),
-                }))
-            }
-        }
-        Continue => {
-            if let Some(TrackingEvent::Stop { .. }) = data.last() {
-                if let Some(TrackingEvent::Start(TrackingData { description, .. })) = data
-                    .iter()
-                    .rev()
-                    .skip_while(|t| t.is_stop())
-                    .next()
-                    .cloned()
-                {
-                    data.push(TrackingEvent::Start(TrackingData {
-                        description,
-                        time: Local::now().into(),
-                    }))
-                }
-            } else {
-                eprintln!("Time tracking couldn't be continued, because there are no entries. Use the start command instead!");
-            }
-        }
-        List => data.iter().for_each(|e| println!("{:?}", e)),
-        Path => println!("{}", path.to_string_lossy()),
-        Show { start, stop } => {
-            let start = start.map(parse_date_or_date_time);
-            let stop = match stop {
-                Some(s) if s == "all" => None,
-                Some(s) => Some(parse_date_or_date_time(s)),
-                None => match start {
-                    Some(DateOrDateTime::DateTime(start)) => {
-                        Some(DateOrDateTime::Date(start.date()))
-                    }
-                    start => start,
-                },
-            };
-            let mut data_iterator = data
-                .iter()
-                .filter(|entry| match start {
-                    None => true,
-                    Some(DateOrDateTime::Date(start)) => {
-                        entry.time().timestamp_millis()
-                            >= start
-                                .and_time(NaiveTime::from_hms(0, 0, 0))
-                                .timestamp_millis()
-                    }
-                    Some(DateOrDateTime::DateTime(start)) => {
-                        entry.time().timestamp_millis() >= start.timestamp_millis()
-                    }
-                })
-                .filter(|entry| match stop {
-                    None => true,
-                    Some(DateOrDateTime::Date(stop)) => {
-                        entry.time().timestamp_millis()
-                            <= stop
-                                .and_time(NaiveTime::from_hms(23, 59, 59))
-                                .timestamp_millis()
-                    }
-                    Some(DateOrDateTime::DateTime(stop)) => {
-                        entry.time().timestamp_millis() <= stop.timestamp_millis()
-                    }
-                })
-                .skip_while(|entry| TrackingEvent::is_stop(entry));
-            let mut work_day = Duration::zero();
-            loop {
-                let start = data_iterator.next();
-                let stop = data_iterator.next();
-                match (start, stop) {
-                    (Some(start), Some(stop)) => {
-                        let duration = stop.time() - start.time();
-                        work_day = work_day
-                            .checked_add(&duration)
-                            .expect("couldn't add up durations");
-                    }
-                    (Some(start), None) => {
-                        let duration = Utc::now() - start.time();
-                        work_day = work_day
-                            .checked_add(&duration)
-                            .expect("couldn't add up durations");
-                        break;
-                    }
-                    (_, _) => break,
-                }
-            }
-            let hours = work_day.num_hours();
-            let hours_in_minutes = hours * 60;
-            let hours_in_seconds = hours_in_minutes * 60;
-            let minutes = work_day.num_minutes() - hours_in_minutes;
-            let minutes_in_seconds = minutes * 60;
-            let seconds = work_day.num_seconds() - hours_in_seconds - minutes_in_seconds;
-            println!("Work Time: {:02}:{:02}:{:02}", hours, minutes, seconds);
+        Command::Start { description, at } => start_tracking(&mut data, description, at),
+        Command::Stop { description, at } => stop_tracking(&mut data, description, at),
+        Command::Continue => continue_tracking(&mut data),
+        Command::List => data.iter().for_each(|e| println!("{:?}", e)),
+        Command::Path => println!("{}", path.to_string_lossy()),
+        Command::Show { start, stop } => show(&mut data, start, stop),
+        #[cfg(feature = "binary")]
+        Command::Export { path } => {
+            write_data_json(path, &data);
         }
         #[allow(unreachable_patterns)]
         _ => unimplemented!(),
     }
 
-    write_data(path, data);
-    Ok(())
+    write_data(path, &data);
 }
 
-fn parse_date_time(s: String) -> DateTime<Utc> {
-    if let Ok(time) = NaiveTime::parse_from_str(&format!("{}", s), "%H:%M:%S") {
+fn parse_date_time(s: &str) -> DateTime<Utc> {
+    if let Ok(time) = NaiveTime::parse_from_str(s, "%H:%M:%S") {
         let today = Local::today();
         let date_time = today.and_time(time).unwrap();
         return date_time.with_timezone(&Utc);
@@ -262,7 +277,7 @@ fn parse_date_time(s: String) -> DateTime<Utc> {
         let date_time = today.and_time(time).unwrap();
         return date_time.with_timezone(&Utc);
     }
-    if let Ok(date_time) = NaiveDateTime::parse_from_str(&format!("{}", s), "%Y-%m-%d %H:%M:%S") {
+    if let Ok(date_time) = NaiveDateTime::parse_from_str(s, "%Y-%m-%d %H:%M:%S") {
         return TimeZone::from_local_datetime(&Local, &date_time)
             .unwrap()
             .with_timezone(&Utc);
@@ -279,12 +294,12 @@ fn parse_date_time(s: String) -> DateTime<Utc> {
         .with_timezone(&Utc)
 }
 
-fn parse_date_or_date_time(s: String) -> DateOrDateTime {
+fn parse_date_or_date_time(s: &str) -> DateOrDateTime {
     if let Ok(date) = NaiveDate::parse_from_str(&s, "%Y-%m-%d") {
         return DateOrDateTime::Date(date);
     }
-    if let Ok(date) = NaiveDateTime::parse_from_str(&format!("{}", s), "%Y-%m-%d %H:%M:%S")
-        .map(DateOrDateTime::DateTime)
+    if let Ok(date) =
+        NaiveDateTime::parse_from_str(&s, "%Y-%m-%d %H:%M:%S").map(DateOrDateTime::DateTime)
     {
         return date;
     }
